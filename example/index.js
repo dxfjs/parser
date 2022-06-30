eb731fcea21d059210135339cf75df1b3cb03420
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2022-06-30 22:43:47 +0100

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
 const { Parser } = require('../lib');
 const { readFileSync } = require('fs');
 
-const content = readFileSync('./specs/examples/noentities.dxf');
+const content = readFileSync('./specs/__dxfs__/noentities.dxf');
 const parser = new Parser();
 parser
     .parse(content.toString())
-    .then((obj) => console.log(obj.tables.dimStyle.unknowns))
+    .then((obj) => console.log(obj))
     .catch((error) => console.error(error));
