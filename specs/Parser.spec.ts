cfdeb4d1648aa829567be3928e540a7b8b3ca1fb
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2022-06-30 23:21:00 +0100

    Update Parser.spec.ts

--- specs/Parser.spec.ts
+++ specs/Parser.spec.ts
@@ -4,7 +4,7 @@ import { Parser } from '../src';
 const _content = readFileSync('./specs/__dxfs__/noentities.dxf');
 
 describe('Parser class', () => {
-    it('should parse header all header variables', async () => {
+    it('should parse all header variables', async () => {
         const dxfObj = await new Parser().parse(_content.toString());
         expect(Object.values(dxfObj.header).length).toBe(251); // We have 251 variables in noentities.dxf 😉.
     });
